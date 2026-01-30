a95d240d86fbb90c87fb486fa3a4c9beac49118f
houmgaor <github@houmgaor.com>
2026-01-30 11:23:25 +0100

    test: add error path tests for codec factory and constructors
    - Add DefaultCodecFactoryTests.cs testing:
      - Valid encoder creation (RW, HFI, LZ)
      - Valid decoder creation (None, RW, HFIRW, LZ, HFI)
      - InvalidOperationException for unsupported encoder types (None, HFIRW)
      - InvalidOperationException for invalid enum values
      - Type verification for returned codec instances
    
    - Add ConstructorValidationTests.cs testing ArgumentNullException for:
      - PackingService (fileSystem, logger, codecFactory, config)
      - UnpackingService (fileSystem, logger, codecFactory, config)
      - FileProcessingService (fileSystem, logger, config)
      - Pack (packingService)
      - Unpack (unpackingService)
      - Program (fileSystem, logger, config)
      - FileOperations (fileSystem, logger)
      - ArgumentsParser (logger)
    
    Test coverage added: 39 new test cases (286 total)

---
+++ ReFrontier.Tests/ConstructorValidationTests.cs
@@ -0,0 +1,187 @@
+using System;
+using Xunit;
+
+using LibReFrontier;
+using LibReFrontier.Abstractions;
+using ReFrontier.Jpk;
+using ReFrontier.Services;
+using ReFrontier.Tests.Mocks;
+
+namespace ReFrontier.Tests
+{
+    /// <summary>
+    /// Tests for constructor null argument validation across services.
+    /// </summary>
+    public class ConstructorValidationTests
+    {
+        private readonly InMemoryFileSystem _fileSystem = new();
+        private readonly TestLogger _logger = new();
+        private readonly FileProcessingConfig _config = FileProcessingConfig.Default();
+        private readonly ICodecFactory _codecFactory = new DefaultCodecFactory();
+
+        #region PackingService Constructor Tests
+
+        [Fact]
+        public void PackingService_NullFileSystem_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new PackingService(null!, _logger, _codecFactory, _config));
+        }
+
+        [Fact]
+        public void PackingService_NullLogger_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new PackingService(_fileSystem, null!, _codecFactory, _config));
+        }
+
+        [Fact]
+        public void PackingService_NullCodecFactory_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new PackingService(_fileSystem, _logger, null!, _config));
+        }
+
+        [Fact]
+        public void PackingService_NullConfig_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new PackingService(_fileSystem, _logger, _codecFactory, null!));
+        }
+
+        #endregion
+
+        #region UnpackingService Constructor Tests
+
+        [Fact]
+        public void UnpackingService_NullFileSystem_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new UnpackingService(null!, _logger, _codecFactory, _config));
+        }
+
+        [Fact]
+        public void UnpackingService_NullLogger_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new UnpackingService(_fileSystem, null!, _codecFactory, _config));
+        }
+
+        [Fact]
+        public void UnpackingService_NullCodecFactory_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new UnpackingService(_fileSystem, _logger, null!, _config));
+        }
+
+        [Fact]
+        public void UnpackingService_NullConfig_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new UnpackingService(_fileSystem, _logger, _codecFactory, null!));
+        }
+
+        #endregion
+
+        #region FileProcessingService Constructor Tests
+
+        [Fact]
+        public void FileProcessingService_NullFileSystem_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new FileProcessingService(null!, _logger, _config));
+        }
+
+        [Fact]
+        public void FileProcessingService_NullLogger_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new FileProcessingService(_fileSystem, null!, _config));
+        }
+
+        [Fact]
+        public void FileProcessingService_NullConfig_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new FileProcessingService(_fileSystem, _logger, null!));
+        }
+
+        #endregion
+
+        #region Pack Constructor Tests
+
+        [Fact]
+        public void Pack_NullPackingService_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new Pack((PackingService)null!));
+        }
+
+        #endregion
+
+        #region Unpack Constructor Tests
+
+        [Fact]
+        public void Unpack_NullUnpackingService_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new Unpack((UnpackingService)null!));
+        }
+
+        #endregion
+
+        #region Program Constructor Tests
+
+        [Fact]
+        public void Program_NullFileSystem_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new Program(null!, _logger, _codecFactory, _config));
+        }
+
+        [Fact]
+        public void Program_NullLogger_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new Program(_fileSystem, null!, _codecFactory, _config));
+        }
+
+        [Fact]
+        public void Program_NullConfig_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new Program(_fileSystem, _logger, _codecFactory, null!));
+        }
+
+        #endregion
+
+        #region FileOperations Constructor Tests
+
+        [Fact]
+        public void FileOperations_NullFileSystem_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new FileOperations(null!, _logger));
+        }
+
+        [Fact]
+        public void FileOperations_NullLogger_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new FileOperations(_fileSystem, null!));
+        }
+
+        #endregion
+
+        #region ArgumentsParser Constructor Tests
+
+        [Fact]
+        public void ArgumentsParser_NullLogger_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+                new ArgumentsParser(null!));
+        }
+
+        #endregion
+    }
+}
---
+++ ReFrontier.Tests/Jpk/DefaultCodecFactoryTests.cs
@@ -0,0 +1,166 @@
+using System;
+using Xunit;
+
+using LibReFrontier;
+using ReFrontier.Jpk;
+
+namespace ReFrontier.Tests.Jpk
+{
+    /// <summary>
+    /// Tests for DefaultCodecFactory.
+    /// </summary>
+    public class DefaultCodecFactoryTests
+    {
+        private readonly DefaultCodecFactory _factory;
+
+        public DefaultCodecFactoryTests()
+        {
+            _factory = new DefaultCodecFactory();
+        }
+
+        #region CreateEncoder Tests
+
+        [Theory]
+        [InlineData(CompressionType.RW)]
+        [InlineData(CompressionType.HFI)]
+        [InlineData(CompressionType.LZ)]
+        public void CreateEncoder_ValidType_ReturnsEncoder(CompressionType type)
+        {
+            // Act
+            var encoder = _factory.CreateEncoder(type);
+
+            // Assert
+            Assert.NotNull(encoder);
+            Assert.IsAssignableFrom<IJPKEncode>(encoder);
+        }
+
+        [Fact]
+        public void CreateEncoder_NoneType_ThrowsInvalidOperationException()
+        {
+            // Act & Assert
+            var exception = Assert.Throws<InvalidOperationException>(() =>
+                _factory.CreateEncoder(CompressionType.None));
+
+            Assert.Contains("Unsupported", exception.Message);
+            Assert.Contains("None", exception.Message);
+        }
+
+        [Fact]
+        public void CreateEncoder_HFIRWType_ThrowsInvalidOperationException()
+        {
+            // HFIRW encoding is not supported
+            // Act & Assert
+            var exception = Assert.Throws<InvalidOperationException>(() =>
+                _factory.CreateEncoder(CompressionType.HFIRW));
+
+            Assert.Contains("Unsupported", exception.Message);
+        }
+
+        [Fact]
+        public void CreateEncoder_InvalidEnumValue_ThrowsInvalidOperationException()
+        {
+            // Act & Assert
+            var exception = Assert.Throws<InvalidOperationException>(() =>
+                _factory.CreateEncoder((CompressionType)999));
+
+            Assert.Contains("Unsupported", exception.Message);
+        }
+
+        #endregion
+
+        #region CreateDecoder Tests
+
+        [Theory]
+        [InlineData(CompressionType.None)]
+        [InlineData(CompressionType.RW)]
+        [InlineData(CompressionType.HFIRW)]
+        [InlineData(CompressionType.LZ)]
+        [InlineData(CompressionType.HFI)]
+        public void CreateDecoder_ValidType_ReturnsDecoder(CompressionType type)
+        {
+            // Act
+            var decoder = _factory.CreateDecoder(type);
+
+            // Assert
+            Assert.NotNull(decoder);
+            Assert.IsAssignableFrom<IJPKDecode>(decoder);
+        }
+
+        [Fact]
+        public void CreateDecoder_InvalidEnumValue_ThrowsInvalidOperationException()
+        {
+            // Act & Assert
+            var exception = Assert.Throws<InvalidOperationException>(() =>
+                _factory.CreateDecoder((CompressionType)999));
+
+            Assert.Contains("Unsupported", exception.Message);
+        }
+
+        #endregion
+
+        #region Encoder Type Verification
+
+        [Fact]
+        public void CreateEncoder_RW_ReturnsJPKEncodeRW()
+        {
+            var encoder = _factory.CreateEncoder(CompressionType.RW);
+            Assert.IsType<JPKEncodeRW>(encoder);
+        }
+
+        [Fact]
+        public void CreateEncoder_HFI_ReturnsJPKEncodeHFI()
+        {
+            var encoder = _factory.CreateEncoder(CompressionType.HFI);
+            Assert.IsType<JPKEncodeHFI>(encoder);
+        }
+
+        [Fact]
+        public void CreateEncoder_LZ_ReturnsJPKEncodeLz()
+        {
+            var encoder = _factory.CreateEncoder(CompressionType.LZ);
+            Assert.IsType<JPKEncodeLz>(encoder);
+        }
+
+        #endregion
+
+        #region Decoder Type Verification
+
+        [Fact]
+        public void CreateDecoder_RW_ReturnsJPKDecodeRW()
+        {
+            var decoder = _factory.CreateDecoder(CompressionType.RW);
+            Assert.IsType<JPKDecodeRW>(decoder);
+        }
+
+        [Fact]
+        public void CreateDecoder_None_ReturnsJPKDecodeRW()
+        {
+            // None type uses RW decoder (raw bytes)
+            var decoder = _factory.CreateDecoder(CompressionType.None);
+            Assert.IsType<JPKDecodeRW>(decoder);
+        }
+
+        [Fact]
+        public void CreateDecoder_HFIRW_ReturnsJPKDecodeHFIRW()
+        {
+            var decoder = _factory.CreateDecoder(CompressionType.HFIRW);
+            Assert.IsType<JPKDecodeHFIRW>(decoder);
+        }
+
+        [Fact]
+        public void CreateDecoder_LZ_ReturnsJPKDecodeLz()
+        {
+            var decoder = _factory.CreateDecoder(CompressionType.LZ);
+            Assert.IsType<JPKDecodeLz>(decoder);
+        }
+
+        [Fact]
+        public void CreateDecoder_HFI_ReturnsJPKDecodeHFI()
+        {
+            var decoder = _factory.CreateDecoder(CompressionType.HFI);
+            Assert.IsType<JPKDecodeHFI>(decoder);
+        }
+
+        #endregion
+    }
+}
